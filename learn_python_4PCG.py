4f3aab689de8ded388ca2f555472a1f6425358f6
Haoze Pang <haozepang@pc-28-177.customer.ask4.lan>
2023-09-07 14:19:55 +0100

    init

---
+++ learn_python_4PCG.py
@@ -0,0 +1,50 @@
+"""
+06/09/23 haozepangthegoat
+This file is a practice to write data into a python list. I endeavor to avoid using numpy.
+"""
+# initialisation
+delimiter = ', '
+array = []
+comments = []
+num_comments = 0
+num_column = int
+
+# body
+with open('example_data.txt') as file:
+    # returns the number of lines of comments
+    for line in file:
+        pass
+        test = line.split()[0]
+        if test == '%':
+            num_comments += 1
+
+with open('example_data.txt') as file:
+    pass
+    for num_lines, line in enumerate(file):
+        pass
+        if num_lines < num_comments:
+            pass
+            comments.append(line)
+
+with open('example_data.txt') as file:
+    # calculates the number of column
+    # returns a n dimension array with n being the number of column
+    for num_lines, line in enumerate(file):
+        if num_lines == num_comments - 1:
+            break
+    num_column = len(
+        file.readline().split(delimiter)
+    )
+    array = [[] for i in range(num_column)]
+
+with open('example_data.txt') as file:
+    for num_line, line in enumerate(file):
+        line = line.strip()
+        if num_line >= num_comments:
+            for i in range(0, num_column):
+                pass
+                array[i].append(line.split(delimiter)[i])
+
+print(array)
+print(num_comments)
+print(comments)
